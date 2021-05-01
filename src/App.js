a49934b5d93e8be227b16c098390585a2e0681cc
VesnaVucinic <vesna.vucinic68@gmail.com>
2021-05-01 21:49:13 +0100

    add code for manu

--- src/App.js
+++ src/App.js
@@ -1,11 +1,60 @@
-import Coupon from './components/coupon'
+import React from 'react'
+import './coupon.css'
+import { ContextMenu, MenuItem, ContextMenuTrigger } from "react-contextmenu";
+import {FaRegCopy, FaList,FaEllipsisV, FaShareAlt} from 'react-icons/fa'
+import {RiSendPlaneFill, RiDeleteBin6Line} from 'react-icons/ri'
 
-function App() {
-  return (
-    <div className="App">
-        <Coupon />
-    </div>
-  );
+
+
+const Coupon = () => {
+   
+  const copyCoupon = (e, data) => {
+    var coupon = data.copy
+    navigator.clipboard.writeText(coupon)
+    alert(`Coupon code ${coupon} copied to your clipboard`)
+  }
+
+    return (
+        <>
+         <ContextMenuTrigger id="contextmenu">
+            <div className="coupon">MI50</div>
+      </ContextMenuTrigger>
+ 
+      <ContextMenu id="contextmenu">
+        <MenuItem data={{copy: 'MI50'}} onClick={copyCoupon}>
+          <FaRegCopy className="copy"/>
+          <span>Copy</span>
+        </MenuItem>
+        <MenuItem >
+          <FaEllipsisV className="openwith"/>
+          <span>Open with</span>
+        </MenuItem>
+        <MenuItem >
+          <FaList className="watchlist"/>
+          <span>Add to watchlist</span>
+        </MenuItem>
+        <MenuItem>
+          <RiSendPlaneFill className="send"/>
+          <span>Send</span>
+        </MenuItem>
+        <MenuItem>
+          <RiDeleteBin6Line className="delete"/>
+          <span>Delete</span>
+        </MenuItem>
+        <MenuItem>
+          <FaShareAlt className="share"/>
+          <span>Share</span>
+        </MenuItem>
+      </ContextMenu>
+        </>
+    )
 }
 
-export default App;
+export default Coupon
+
+// Note:
+// The id in ContextMenuTrigger and ContextMenu should be the same.
+// React Icons are optional.
+// data attribute contains the coupon code which is then passed to copyCoupon method
+// The onClick method inside MenuItem will pass the data of data attribute to the copyCoupon method which will then use JavaScript navigator.clipboard method to copy the code to the clipboard.
+ 
